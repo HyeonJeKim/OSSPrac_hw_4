ac8f974ceb4941ca70596533174875cdff00b0d1
HyeonJeKim <1fprrlxk@naver.com>
2023-04-05 01:37:24 +0900

    hello re

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print('hello!')
+import random
+num = 0
+
+
+while num <= 5 :
+    dice = random.randint(1,6)
+    if dice %2 == 0 :
+        num += 1
+    print('Dice roll : ', dice)
+    if num == 5 :
+        break
+print('End of rolling dice')
